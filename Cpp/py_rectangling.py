db92255e7893c2ed3d3b6be905082c1baca86390
Ben North <ben@redfrontdoor.org>
2016-07-02 22:00:56 +0100

    Python reference for update_score_1

--- Cpp/py_rectangling.py
+++ Cpp/py_rectangling.py
@@ -13,3 +13,19 @@ class Observations(namedtuple('Observations', ['zeta', 'theta'])):
         z_pwr_x = z ** x
         lr = (1.0 + z_pwr_th * z_pwr_x) / (z_pwr_th + z_pwr_x)
         return np.log(lr) / np.log(z)
+
+class FactorGraphState(namedtuple('FactorGraphState', 'obs score_1 score_2')):
+    """
+    score_1[i, j] is message sent from K1i to check node linking K1i and K2j (and K12ij).
+    score_2[i, j] is message sent from K2j to check node linking K2j and K1i (and K12ij).
+    """
+    def with_score_1_updated(self):
+        """
+        Calculate new values for score_1.
+        Find Mij message received from K1i along the edge which (via check node involving K12ij) comes from K2j.
+        Sum (over j) of these is 'xi'.  Then new score_1[ij] = xi - Mij.
+        """
+        msgs = self.obs.chk_f(self.score_2)
+        msg_sums = np.sum(msgs, axis=1)
+        updated_score_1 = msg_sums[:, None] - msgs
+        return self._replace(score_1=updated_score_1)
