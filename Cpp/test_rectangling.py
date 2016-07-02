1bd7b6a29d04d10a93e9518fbcefeb72b89ac7ac
Ben North <ben@redfrontdoor.org>
2016-07-02 07:28:09 +0100

    Test Eigen/NumPy interchange

--- Cpp/test_rectangling.py
+++ Cpp/test_rectangling.py
@@ -1,3 +1,5 @@
+import numpy as np
+import numpy.testing as nptest
 import rectangling as cr
 
 def test_import():
@@ -5,3 +7,10 @@ def test_import():
     If we get this far, it was successful.
     """
     assert True
+
+def test_matrix_interchange():
+    shape = (7, 13)
+    mx = cr.test_matrix_i(*shape)
+    assert mx.shape == shape
+    assert mx.dtype.kind == 'i'
+    nptest.assert_array_equal(mx, 42)
