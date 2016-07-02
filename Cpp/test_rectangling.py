9621df27f344c91931cf64f454c6479556aa0a5f
Ben North <ben@redfrontdoor.org>
2016-07-02 08:14:22 +0100

    Test with varying 'n left over'

--- Cpp/test_rectangling.py
+++ Cpp/test_rectangling.py
@@ -39,3 +39,12 @@ class TestEngineContext:
     def test_excess_binomial_rnd_bad_input(self, ec):
         pytest.raises_regexp(ValueError, r'must be in \(0, 1\)',
                              ec.excess_binomial_rnd, 2, 3, -0.5, 10)
+
+    @pytest.mark.parametrize('p', [0.00001, 0.99999], ids='01')
+    @pytest.mark.parametrize('n_leftover', range(6))
+    def test_excess_binomial_rnd(self, ec, n_leftover, p):
+        n_excess = ec.excess_binomial_rnd(2, 3, p, 600 + n_leftover)
+        raw_exp_n_excess = np.full((2, 3), 100, dtype='i')
+        raw_exp_n_excess.ravel()[:n_leftover] += 1
+        exp_n_excess = (raw_exp_n_excess if p > 0.5 else -raw_exp_n_excess)
+        nptest.assert_array_equal(n_excess, exp_n_excess)
