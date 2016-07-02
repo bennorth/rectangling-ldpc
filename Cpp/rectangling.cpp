3fb22c568b36d6109d1da6a1950960606b415894
Ben North <ben@redfrontdoor.org>
2016-07-02 07:36:41 +0100

    Add 'double' test matrix

--- Cpp/rectangling.cpp
+++ Cpp/rectangling.cpp
@@ -3,10 +3,16 @@
 #include <pybind11/eigen.h>
 
 using Eigen::MatrixXi;
+using Eigen::MatrixXd;
 
 MatrixXi test_matrix_i(size_t n1, size_t n2)
 {
     return MatrixXi::Constant(n1, n2, 42);
+}
+
+MatrixXd test_matrix_d(size_t n1, size_t n2)
+{
+    return MatrixXd::Constant(n1, n2, 42.0);
 }
 
 
@@ -15,7 +21,8 @@ namespace py = pybind11;
 PYBIND11_PLUGIN(rectangling) {
     py::module m("rectangling", "Colossus rectangling methods");
 
-    m.def("test_matrix_i", &test_matrix_i, "create test matrix of 42s");
+    m.def("test_matrix_i", &test_matrix_i, "create test matrix of 42s")
+     .def("test_matrix_d", &test_matrix_d, "create test matrix of 42.0s");
 
     return m.ptr();
 }
