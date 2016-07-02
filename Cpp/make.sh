b8369ce324c8da2dc66abfebaa4f1a7cfe5f0188
Ben North <ben@redfrontdoor.org>
2016-07-02 21:32:30 +0100

    Build without NDEBUG for now
    Was swallowing errors about mismatched matrix dimensions.

--- Cpp/make.sh
+++ Cpp/make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-opts_from_python=$(python3.5-config --cflags --ldflags | sed 's/-Wstrict-prototypes//g')
+opts_from_python=$(python3.5-config --cflags --ldflags | sed 's/-Wstrict-prototypes//g' | sed 's/-DNDEBUG//g')
 
 g++ -std=c++11 -Wall -fPIC -O3 -shared \
     -I$CONDA_ENV_PATH/include $opts_from_python \
