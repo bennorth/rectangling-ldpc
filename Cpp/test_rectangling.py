b58e9cb8539a7e5ca3b0536e9661ab041d24318a
Ben North <ben@redfrontdoor.org>
2016-07-05 23:12:51 +0100

    fgs(): Remove now-unused fixture

--- Cpp/test_rectangling.py
+++ Cpp/test_rectangling.py
@@ -186,24 +186,6 @@ class TestFactorGraphState(TestDecodingState):
         rnd_scores_2 = engine_context.unit_normal_shaped_like(sample_obs.theta)
         return cr.FactorGraphState(sample_obs, rnd_scores_1, rnd_scores_2)
 
-    @pytest.fixture
-    def fgs(self, engine_context, sample_obs, request):
-        # Create both and then only return the requested one to avoid
-        # getting the same values out of the randomness engine.
-
-        rnd_scores_1 = engine_context.unit_normal_shaped_like(sample_obs.theta)
-        rnd_scores_2 = engine_context.unit_normal_shaped_like(sample_obs.theta)
-        sample_fgs = cr.FactorGraphState(sample_obs, rnd_scores_1, rnd_scores_2)
-
-        all_fgs = {'sample': sample_fgs,
-                   'random': engine_context.make_FactorGraphState(sample_obs)}
-
-        # Check we really did get different scores
-        score_1_diff = all_fgs['sample'].score_1 - all_fgs['random'].score_1
-        assert np.min(np.abs(score_1_diff)) > 0.1
-
-        return all_fgs[request.param]
-
     def random_scores(self, engine_context, sample_obs):
         rnd_scores_1 = engine_context.unit_normal_shaped_like(sample_obs.theta)
         rnd_scores_2 = engine_context.unit_normal_shaped_like(sample_obs.theta)
