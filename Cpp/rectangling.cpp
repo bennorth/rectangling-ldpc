1df0af49e80f15601071304054a85f942f5e8103
Ben North <ben@redfrontdoor.org>
2016-07-15 00:39:26 +0100

    (Re-order chunks of cpp file)
    Large diff but no actual changes.

--- Cpp/rectangling.cpp
+++ Cpp/rectangling.cpp
@@ -91,6 +91,227 @@ MatrixXd unit_normal_shaped_like(rnd_engine_t& rnd, const MatrixXi& theta)
 VectorXi pattern_from_score(const VectorXd& scores)
 { return scores.unaryExpr([](double x) { return (x > 0.0) ? 1 : 0; }).cast<int>(); }
 
+
+////////////////////////////////////////////////////////////////////////
+
+/*
+  Discrete Dirichlet distribution
+
+  Paper has everything in terms of positive integers, but I think the
+  implementation become simpler if we think of it as non-negative,
+  adjusting everything afterwards.  I.e., an original 'required sum' of
+  20 from 8 numbers in {1, 2, 3, 4} because a required sum of 20-8 = 12
+  from 8 numbers in {0, 1, 2, 3}.  Then the user can add one to each
+  term afterwards.
+*/
+
+////////////////////////////////////////////////////////////////////////
+
+class DirichletState
+{
+public:
+    enum class Bound { Lower, Upper };
+    DirichletState(size_t n_terms, size_t max_term, size_t required_sum, Bound bound);
+    DirichletState(const VectorXu& terms, size_t max_term);
+
+    const VectorXu& terms() const { return terms_; }
+    void mutate(double x);
+    void mutate_many(const std::vector<double>& xs);
+
+private:
+    const size_t n_terms_;
+    const size_t max_term_;
+    const size_t required_sum_;
+    VectorXu terms_;
+};
+
+DirichletState::DirichletState(size_t n_terms, size_t max_term, size_t required_sum, Bound bound)
+    : n_terms_(n_terms), max_term_(max_term), required_sum_(required_sum)
+{
+    if (n_terms * max_term < required_sum)
+        throw std::range_error("inconsistent arguments: required_sum too large");
+
+    size_t n_maximal = required_sum / max_term;
+    size_t left_over = required_sum % max_term;
+
+    terms_.resize(n_terms);
+    for (size_t i = 0; i < n_maximal; ++i)
+        terms_(i) = max_term;
+
+    // If it exactly fits (e.g., 12 = 4 + 4 + 4), there are no more
+    // terms to populate, and also Upper and Lower are equal, so we're
+    // finished.
+    if (n_maximal == n_terms)
+        return;
+
+    for (size_t i = n_maximal + 1; i < n_terms; ++i)
+        terms_(i) = 0;
+
+    terms_(n_maximal) = left_over;
+
+    if (bound == Bound::Lower)
+        terms_.reverseInPlace();
+}
+
+DirichletState::DirichletState(const VectorXu& terms, size_t max_term)
+    : n_terms_(terms.rows()), max_term_(max_term), required_sum_(terms.sum()), terms_(terms)
+{
+}
+
+void DirichletState::mutate(double x)
+{
+    if ((x < 0.0) || (x >= static_cast<double>(n_terms_ - 1)))
+        throw std::range_error("x out of range [0, n-1)");
+
+    double integer_part;
+    double fractional_part = std::modf(x, &integer_part);
+
+    size_t first_elt_idx = static_cast<size_t>(integer_part);
+    size_t pair_sum = terms_(first_elt_idx) + terms_(first_elt_idx + 1);
+
+    size_t min_choice_0 = (pair_sum < max_term_) ? 0 : (pair_sum - max_term_);
+    size_t max_choice_0 = std::min(pair_sum, max_term_);
+    size_t n_choices = max_choice_0 - min_choice_0 + 1;
+
+    size_t choice_offset = static_cast<size_t>(n_choices * fractional_part);
+    size_t choice_0 = min_choice_0 + choice_offset;
+
+    terms_(first_elt_idx) = choice_0;
+    terms_(first_elt_idx + 1) = pair_sum - choice_0;
+}
+
+void DirichletState::mutate_many(const std::vector<double>& xs)
+{
+    size_t n_elts = xs.size();
+    while (n_elts)
+        mutate(xs[--n_elts]);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+class DirichletSamplingState
+{
+public:
+    DirichletSamplingState(size_t n_terms, size_t max_term, size_t required_sum);
+    VectorXu maybe_coalesced_result() const;
+    bool has_coalesced() const { return has_coalesced_; }
+    size_t lambda_size() const { return vec_lambda_.size(); }
+    size_t n_terms() const { return n_terms_; }
+    void extend_lambda_vec(const VectorXd& more_lambda);
+
+private:
+    const size_t n_terms_;
+    const size_t max_term_;
+    const size_t required_sum_;
+    std::vector<double> vec_lambda_;
+    VectorXu maybe_coalesced_result_;
+    bool has_coalesced_;
+
+    void determine_coalescence_();
+};
+
+DirichletSamplingState::DirichletSamplingState(size_t n_terms, size_t max_term, size_t required_sum)
+    : n_terms_(n_terms), max_term_(max_term), required_sum_(required_sum)
+{
+    determine_coalescence_();
+}
+
+void DirichletSamplingState::determine_coalescence_()
+{
+    DirichletState x_lwr(n_terms_, max_term_, required_sum_,
+                         DirichletState::Bound::Lower);
+    DirichletState x_upr(n_terms_, max_term_, required_sum_,
+                         DirichletState::Bound::Upper);
+
+    x_lwr.mutate_many(vec_lambda_);
+    x_upr.mutate_many(vec_lambda_);
+
+    // Set this anyway; it will be ignored if they're not the same.
+    maybe_coalesced_result_ = x_lwr.terms();
+    has_coalesced_ = (x_lwr.terms() == x_upr.terms());
+}
+
+VectorXu DirichletSamplingState::maybe_coalesced_result() const
+{
+    if (has_coalesced_)
+        return maybe_coalesced_result_;
+    else
+        return VectorXu();
+}
+
+void DirichletSamplingState::extend_lambda_vec(const VectorXd& more_lambda)
+{
+    const double* lambda_begin = more_lambda.data();
+    const double* lambda_end = lambda_begin + more_lambda.size();
+
+    vec_lambda_.insert(vec_lambda_.end(), lambda_begin, lambda_end);
+    determine_coalescence_();
+}
+
+////////////////////////////////////////////////////////////////////////
+
+class DirichletSamplingRun
+{
+public:
+    DirichletSamplingRun(rnd_engine_t& rnd,
+                         size_t n_terms, size_t max_term, size_t required_sum);
+
+    VectorXu result() const { return state_.maybe_coalesced_result(); }
+
+private:
+    rnd_engine_t& rnd_;
+    DirichletSamplingState state_;
+};
+
+DirichletSamplingRun::DirichletSamplingRun(rnd_engine_t& rnd,
+                                           size_t n_terms, size_t max_term, size_t required_sum)
+    : rnd_(rnd), state_(n_terms, max_term, required_sum)
+{
+    while ( ! state_.has_coalesced()) {
+        size_t required_n_new = std::max(static_cast<size_t>(1), state_.lambda_size());
+        VectorXd new_lambda = uniform_of_size(rnd_,
+                                              required_n_new,
+                                              static_cast<double>(state_.n_terms() - 1));
+        state_.extend_lambda_vec(new_lambda);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+
+/*
+  Generating legal wheel patterns for Chi1 (41 cams) and Chi2 (31 cams)
+
+  D-Chi1 must have as near to 20.5 crosses as possible, while having an
+  even number of crosses.  So it must have 20 crosses.
+
+  D-Chi2 similarly must have 16 crosses.
+
+  The un-delta'd wheels then must have:
+
+  Chi1: 20 crosses and 21 dots
+     or 21 crosses and 20 dots;
+
+  Chi2: 15 crosses and 16 dots
+     or 16 crosses and 15 dots.
+
+  Cannot have more than four consecutive like characters in the
+  un-delta'd wheel, i.e., no more than three consecutive dots in the
+  delta'd-wheel.
+
+  [25D(e), p.148]
+
+  Adapting the analysis of [25X, p.183] to Chi1:
+
+  Chi1 must consist of 10 blocks of cross and 10 blocks of dot.  Each
+  consists of from 1 to 4 characters.  Either the sum of the cross-block
+  sizes must be 20 and the sum of the dot-block sizes 21, or the other
+  way round.  Therefore we choose ten numbers from {1, 2, 3, 4} which
+  sum to 20, and ten which sum to 21, and interleave them.  Then choose
+  whether we start with a dot- or cross-block.  Then choose a random
+  starting position.  (For sampling, we can ignore the redundancy by a
+  factor of 10 arising from choosing which block to start with.)
+ */
+
 ////////////////////////////////////////////////////////////////////////
 
 class Patterns
@@ -370,222 +591,6 @@ bool update_until_convergence(State& state, size_t n_same_converged, size_t max_
 
 ////////////////////////////////////////////////////////////////////////
 
-/*
-  Generating legal wheel patterns for Chi1 (41 cams) and Chi2 (31 cams)
-
-  D-Chi1 must have as near to 20.5 crosses as possible, while having an
-  even number of crosses.  So it must have 20 crosses.
-
-  D-Chi2 similarly must have 16 crosses.
-
-  The un-delta'd wheels then must have:
-
-  Chi1: 20 crosses and 21 dots
-     or 21 crosses and 20 dots;
-
-  Chi2: 15 crosses and 16 dots
-     or 16 crosses and 15 dots.
-
-  Cannot have more than four consecutive like characters in the
-  un-delta'd wheel, i.e., no more than three consecutive dots in the
-  delta'd-wheel.
-
-  [25D(e), p.148]
-
-  Adapting the analysis of [25X, p.183] to Chi1:
-
-  Chi1 must consist of 10 blocks of cross and 10 blocks of dot.  Each
-  consists of from 1 to 4 characters.  Either the sum of the cross-block
-  sizes must be 20 and the sum of the dot-block sizes 21, or the other
-  way round.  Therefore we choose ten numbers from {1, 2, 3, 4} which
-  sum to 20, and ten which sum to 21, and interleave them.  Then choose
-  whether we start with a dot- or cross-block.  Then choose a random
-  starting position.  (For sampling, we can ignore the redundancy by a
-  factor of 10 arising from choosing which block to start with.)
- */
-
-/*
-  Discrete Dirichlet distribution
-
-  Paper has everything in terms of positive integers, but I think the
-  implementation become simpler if we think of it as non-negative,
-  adjusting everything afterwards.  I.e., an original 'required sum' of
-  20 from 8 numbers in {1, 2, 3, 4} because a required sum of 20-8 = 12
-  from 8 numbers in {0, 1, 2, 3}.  Then the user can add one to each
-  term afterwards.
-*/
-
-class DirichletState
-{
-public:
-    enum class Bound { Lower, Upper };
-    DirichletState(size_t n_terms, size_t max_term, size_t required_sum, Bound bound);
-    DirichletState(const VectorXu& terms, size_t max_term);
-
-    const VectorXu& terms() const { return terms_; }
-    void mutate(double x);
-    void mutate_many(const std::vector<double>& xs);
-
-private:
-    const size_t n_terms_;
-    const size_t max_term_;
-    const size_t required_sum_;
-    VectorXu terms_;
-};
-
-DirichletState::DirichletState(size_t n_terms, size_t max_term, size_t required_sum, Bound bound)
-    : n_terms_(n_terms), max_term_(max_term), required_sum_(required_sum)
-{
-    if (n_terms * max_term < required_sum)
-        throw std::range_error("inconsistent arguments: required_sum too large");
-
-    size_t n_maximal = required_sum / max_term;
-    size_t left_over = required_sum % max_term;
-
-    terms_.resize(n_terms);
-    for (size_t i = 0; i < n_maximal; ++i)
-        terms_(i) = max_term;
-
-    // If it exactly fits (e.g., 12 = 4 + 4 + 4), there are no more
-    // terms to populate, and also Upper and Lower are equal, so we're
-    // finished.
-    if (n_maximal == n_terms)
-        return;
-
-    for (size_t i = n_maximal + 1; i < n_terms; ++i)
-        terms_(i) = 0;
-
-    terms_(n_maximal) = left_over;
-
-    if (bound == Bound::Lower)
-        terms_.reverseInPlace();
-}
-
-DirichletState::DirichletState(const VectorXu& terms, size_t max_term)
-    : n_terms_(terms.rows()), max_term_(max_term), required_sum_(terms.sum()), terms_(terms)
-{
-}
-
-void DirichletState::mutate(double x)
-{
-    if ((x < 0.0) || (x >= static_cast<double>(n_terms_ - 1)))
-        throw std::range_error("x out of range [0, n-1)");
-
-    double integer_part;
-    double fractional_part = std::modf(x, &integer_part);
-
-    size_t first_elt_idx = static_cast<size_t>(integer_part);
-    size_t pair_sum = terms_(first_elt_idx) + terms_(first_elt_idx + 1);
-
-    size_t min_choice_0 = (pair_sum < max_term_) ? 0 : (pair_sum - max_term_);
-    size_t max_choice_0 = std::min(pair_sum, max_term_);
-    size_t n_choices = max_choice_0 - min_choice_0 + 1;
-
-    size_t choice_offset = static_cast<size_t>(n_choices * fractional_part);
-    size_t choice_0 = min_choice_0 + choice_offset;
-
-    terms_(first_elt_idx) = choice_0;
-    terms_(first_elt_idx + 1) = pair_sum - choice_0;
-}
-
-void DirichletState::mutate_many(const std::vector<double>& xs)
-{
-    size_t n_elts = xs.size();
-    while (n_elts)
-        mutate(xs[--n_elts]);
-}
-
-////////////////////////////////////////////////////////////////////////
-
-class DirichletSamplingState
-{
-public:
-    DirichletSamplingState(size_t n_terms, size_t max_term, size_t required_sum);
-    VectorXu maybe_coalesced_result() const;
-    bool has_coalesced() const { return has_coalesced_; }
-    size_t lambda_size() const { return vec_lambda_.size(); }
-    size_t n_terms() const { return n_terms_; }
-    void extend_lambda_vec(const VectorXd& more_lambda);
-
-private:
-    const size_t n_terms_;
-    const size_t max_term_;
-    const size_t required_sum_;
-    std::vector<double> vec_lambda_;
-    VectorXu maybe_coalesced_result_;
-    bool has_coalesced_;
-
-    void determine_coalescence_();
-};
-
-DirichletSamplingState::DirichletSamplingState(size_t n_terms, size_t max_term, size_t required_sum)
-    : n_terms_(n_terms), max_term_(max_term), required_sum_(required_sum)
-{
-    determine_coalescence_();
-}
-
-void DirichletSamplingState::determine_coalescence_()
-{
-    DirichletState x_lwr(n_terms_, max_term_, required_sum_,
-                         DirichletState::Bound::Lower);
-    DirichletState x_upr(n_terms_, max_term_, required_sum_,
-                         DirichletState::Bound::Upper);
-
-    x_lwr.mutate_many(vec_lambda_);
-    x_upr.mutate_many(vec_lambda_);
-
-    // Set this anyway; it will be ignored if they're not the same.
-    maybe_coalesced_result_ = x_lwr.terms();
-    has_coalesced_ = (x_lwr.terms() == x_upr.terms());
-}
-
-VectorXu DirichletSamplingState::maybe_coalesced_result() const
-{
-    if (has_coalesced_)
-        return maybe_coalesced_result_;
-    else
-        return VectorXu();
-}
-
-void DirichletSamplingState::extend_lambda_vec(const VectorXd& more_lambda)
-{
-    const double* lambda_begin = more_lambda.data();
-    const double* lambda_end = lambda_begin + more_lambda.size();
-
-    vec_lambda_.insert(vec_lambda_.end(), lambda_begin, lambda_end);
-    determine_coalescence_();
-}
-
-////////////////////////////////////////////////////////////////////////
-
-class DirichletSamplingRun
-{
-public:
-    DirichletSamplingRun(rnd_engine_t& rnd,
-                         size_t n_terms, size_t max_term, size_t required_sum);
-
-    VectorXu result() const { return state_.maybe_coalesced_result(); }
-
-private:
-    rnd_engine_t& rnd_;
-    DirichletSamplingState state_;
-};
-
-DirichletSamplingRun::DirichletSamplingRun(rnd_engine_t& rnd,
-                                           size_t n_terms, size_t max_term, size_t required_sum)
-    : rnd_(rnd), state_(n_terms, max_term, required_sum)
-{
-    while ( ! state_.has_coalesced()) {
-        size_t required_n_new = std::max(static_cast<size_t>(1), state_.lambda_size());
-        VectorXd new_lambda = uniform_of_size(rnd_,
-                                              required_n_new,
-                                              static_cast<double>(state_.n_terms() - 1));
-        state_.extend_lambda_vec(new_lambda);
-    }
-}
-
-////////////////////////////////////////////////////////////////////////
-
 class EngineContext
 {
 public:
@@ -642,6 +647,33 @@ PYBIND11_PLUGIN(rectangling) {
         .def("test_matrix_d", &test_matrix_d, "create test matrix of 42.0s")
         ;
 
+    py::class_<DirichletState> cls_DirichletState(rect_module, "DirichletState");
+    cls_DirichletState
+        .def(py::init<const VectorXu&, size_t>())
+        .def(py::init<size_t, size_t, size_t, DirichletState::Bound>())
+        .def_property_readonly("terms", &DirichletState::terms)
+        .def("mutate", &DirichletState::mutate)
+        .def("mutate_many", &DirichletState::mutate_many)
+        ;
+
+    py::enum_<DirichletState::Bound>(cls_DirichletState, "Bound")
+        .value("Lower", DirichletState::Bound::Lower)
+        .value("Upper", DirichletState::Bound::Upper)
+        ;
+
+    py::class_<DirichletSamplingState>(rect_module, "DirichletSamplingState")
+        .def(py::init<size_t, size_t, size_t>())
+        .def("maybe_coalesced_result", &DirichletSamplingState::maybe_coalesced_result)
+        .def_property_readonly("has_coalesced", &DirichletSamplingState::has_coalesced)
+        .def_property_readonly("lambda_size", &DirichletSamplingState::lambda_size)
+        .def_property_readonly("n_terms", &DirichletSamplingState::n_terms)
+        .def("extend_lambda_vec", &DirichletSamplingState::extend_lambda_vec)
+        ;
+
+    py::class_<DirichletSamplingRun>(rect_module, "DirichletSamplingRun")
+        .def_property_readonly("result", &DirichletSamplingRun::result)
+        ;
+
     py::class_<Patterns>(rect_module, "Patterns")
         .def(py::init<const VectorXi&, const VectorXi&>())
         .def_readonly("chi1", &Patterns::chi1)
@@ -680,33 +712,6 @@ PYBIND11_PLUGIN(rectangling) {
         .def_property_readonly("pattern_2", &AccurateConvergenceState::pattern_2)
         .def("update_score_1", &AccurateConvergenceState::update_score_1)
         .def("update_score_2", &AccurateConvergenceState::update_score_2)
-        ;
-
-    py::class_<DirichletState> cls_DirichletState(rect_module, "DirichletState");
-    cls_DirichletState
-        .def(py::init<const VectorXu&, size_t>())
-        .def(py::init<size_t, size_t, size_t, DirichletState::Bound>())
-        .def_property_readonly("terms", &DirichletState::terms)
-        .def("mutate", &DirichletState::mutate)
-        .def("mutate_many", &DirichletState::mutate_many)
-        ;
-
-    py::enum_<DirichletState::Bound>(cls_DirichletState, "Bound")
-        .value("Lower", DirichletState::Bound::Lower)
-        .value("Upper", DirichletState::Bound::Upper)
-        ;
-
-    py::class_<DirichletSamplingState>(rect_module, "DirichletSamplingState")
-        .def(py::init<size_t, size_t, size_t>())
-        .def("maybe_coalesced_result", &DirichletSamplingState::maybe_coalesced_result)
-        .def_property_readonly("has_coalesced", &DirichletSamplingState::has_coalesced)
-        .def_property_readonly("lambda_size", &DirichletSamplingState::lambda_size)
-        .def_property_readonly("n_terms", &DirichletSamplingState::n_terms)
-        .def("extend_lambda_vec", &DirichletSamplingState::extend_lambda_vec)
-        ;
-
-    py::class_<DirichletSamplingRun>(rect_module, "DirichletSamplingRun")
-        .def_property_readonly("result", &DirichletSamplingRun::result)
         ;
 
     py::class_<EngineContext>(rect_module, "EngineContext")
