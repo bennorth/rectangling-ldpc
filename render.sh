ea4f65cd9e37e889201672415f55c11fd56bc85c
Ben North <ben@redfrontdoor.org>
2016-09-11 17:55:36 +0100

    Render SVGs to PNG

--- render.sh
+++ render.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 pandoc content.md -c hugo-octopress.css -s --mathjax -o index.html
+inkscape factor-graph-chi.svg --export-png=factor-graph-chi.png --export-width=480
+inkscape factor-graph-one-chk.svg --export-png=factor-graph-one-chk.png --export-width=420
+
 refresh-chrome content.html
