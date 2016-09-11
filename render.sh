51c0659c111b3d2a56daa445bf111609723b4480
Ben North <ben@redfrontdoor.org>
2016-09-11 18:16:49 +0100

    Refresh newly-titled page on render
    (Script was stale since move to 'index.html' in fact.)

--- render.sh
+++ render.sh
@@ -4,4 +4,4 @@ pandoc content.md -c hugo-octopress.css -s --mathjax -o index.html
 inkscape factor-graph-chi.svg --export-png=factor-graph-chi.png --export-width=480
 inkscape factor-graph-one-chk.svg --export-png=factor-graph-one-chk.png --export-width=420
 
-refresh-chrome content.html
+refresh-chrome "Belief Propagation"
